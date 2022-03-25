aad18db1d1f1507009f07054a60f65a6589d7bb2
flora619 <wanghong970619@163.com>
2022-03-25 16:04:42 +0800

    first commit

--- cell_ae.py
+++ cell_ae.py
@@ -23,7 +23,9 @@ def train_ae(model,trainLoader,test_feature):
     start = datetime.datetime.now()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     loss_func = nn.MSELoss()
-    for epoch in range(1, 2000 + 1):
+    best_model=model
+    best_loss=100
+    for epoch in range(1, 2500 + 1 ):
 
         for x in trainLoader:
             y=x
@@ -36,19 +38,22 @@ def train_ae(model,trainLoader,test_feature):
             y = test_feature
             encoded, decoded = model(test_feature)
             test_loss = loss_func(decoded, y)
+        if (test_loss.item() < best_loss):
+            best_loss = test_loss
+            best_model = model
         if epoch%100==0:
-            ind = random.sample(range(388), 10)
             end = datetime.datetime.now()
             print('epoch:' ,epoch, 'train loss = ' ,train_loss.item(),"test loss:",test_loss.item(), "time:",(end - start).seconds)
+    return best_model
 
 lr=0.0001
-batch_size=20
+batch_size=388
 def main():
+    random.seed(4)
     # load  gene expression data, and DNA copy number data of cell line
     RNAseq_feature = pd.read_csv(cell_RNAseq_file, sep=',', header=None, index_col=[0], skiprows=2)
     copynumber_feature = pd.read_csv(cell_copynumber_file, sep=',', header=None, index_col=[0], skiprows=5)
 
-    print(RNAseq_feature.shape)
     cell_index = pd.read_csv(cell_index_file, sep=',', header=None, index_col=[0])
 
     RNAseq_feature = RNAseq_feature.loc[list(cell_index.index)].values
@@ -62,16 +67,18 @@ def main():
 
     RNAseq_indim = RNAseq_feature.shape[-1]
     copynumber_indim = copynumber_feature.shape[-1]
+    print(RNAseq_indim)
+    print(copynumber_indim)
 
-    #dimension reduction(gene expression data)
+    # dimension reduction(gene expression data)
     RNAseq_ae = Auto_Encoder(RNAseq_indim, 400)
     train_list = random.sample((RNAseq_feature).tolist(), int(0.9 * len(RNAseq_feature)))
     test_list = [item for item in (RNAseq_feature).tolist() if item not in train_list]
     train=torch.tensor(train_list).float().to(device)
     test = torch.tensor(test_list).float().to(device)
     data_iter = Data.DataLoader(train, batch_size, shuffle=True)
-    train_ae(RNAseq_ae,data_iter,test)
-    torch.save(RNAseq_ae.output(RNAseq_feature),cell_RNAseq_ae)
+    best_model=train_ae(RNAseq_ae,data_iter,test)
+    torch.save(best_model.output(RNAseq_feature),cell_RNAseq_ae)
 
     # dimension reduction(DNA copy number data)
     copynumber_ae = Auto_Encoder(copynumber_indim, 400)
@@ -81,7 +88,7 @@ def main():
     test = torch.tensor(test_list).float().to(device)
     data_iter = Data.DataLoader(train, batch_size, shuffle=True)
     train_ae(copynumber_ae, data_iter, test)
-    torch.save(copynumber_ae.output(RNAseq_feature), cell_copynumber_ae)
+    torch.save(copynumber_ae.output(copynumber_feature), cell_copynumber_ae)
 
 
 
