6764915b36e13786abb70e60631908643dc2fd99
flora619 <101967463+flora619@users.noreply.github.com>
2022-05-26 10:36:49 +0800

    Update drug_cell_encoder.py

--- model/drug_cell_encoder.py
+++ model/drug_cell_encoder.py
@@ -9,12 +9,16 @@ class Drug_cell_encoder(torch.nn.Module):
         super(Drug_cell_encoder, self).__init__()
         self.gcn=GraphConvolution(indim,indim,device=device)
         self.relu = nn.ReLU(inplace=True)
-        self.dropout=nn.Dropout(0.1)
+        self.dropout=nn.Dropout(0.2)
     def forward(self,drug_cell_pair_feature,edge_idx):
-        output=self.relu(self.gcn(drug_cell_pair_feature,drug_cell_pair_feature,edge_idx,0.1))
-        output=self.relu(self.gcn(drug_cell_pair_feature,output,edge_idx,0.1))
-        output = self.relu(self.gcn(drug_cell_pair_feature, output,edge_idx, 0.1))
-        output = self.relu(self.gcn(drug_cell_pair_feature, output,edge_idx, 0.1))
-        output = self.relu(self.gcn(drug_cell_pair_feature, output,edge_idx, 0.1))
+        output1=self.relu(self.gcn(drug_cell_pair_feature,drug_cell_pair_feature,edge_idx,0.1))
+        output1=self.dropout(output1)
+        output2=self.relu(self.gcn(drug_cell_pair_feature,output1,edge_idx,0.1))
+        output3 = self.relu(self.gcn(drug_cell_pair_feature, output2,edge_idx, 0.1))
+        output3 = self.dropout(output3)
+        output4 = self.relu(self.gcn(drug_cell_pair_feature, output3,edge_idx, 0.1))
+        output5 = self.relu(self.gcn(drug_cell_pair_feature, output4,edge_idx, 0.1))
+        output5 = self.dropout(output5)
 
-        return output
+
+        return output1,output2,output3,output4,output5
